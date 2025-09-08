c2aa809ee7b354e549bbf937d14f71d886668b7b
gordofreemo <shukshinaalinochka@gmail.com>
2025-09-08 10:36:57 +0200

    added setup file

---
+++ setup.sh
@@ -0,0 +1,18 @@
+cd  /path/to/astra-sim
+ASTRA_SIM="$PWD"
+git config --global --add safe.directory "*"
+git submodule update --init --recursive
+
+python3 -m venv chakra_env
+source chakra_env/bin/activate
+
+cd ${ASTRA_SIM}/extern/graph_frontend/chakra
+git submodule update --init --recursive
+
+
+pip3 install .
+
+
+cd ${ASTRA_SIM}
+pip3 install --upgrade 'protobuf==6.31.1'
+./build/astra_analytical/build.sh
