3ce652d8073bdb4982f33033028d50fff809c92e
gordofreemo <shukshinaalinochka@gmail.com>
2025-09-10 13:35:35 +0200

    aded symbolic_tensor pull to setup

--- setup.sh
+++ setup.sh
@@ -10,8 +10,16 @@ git submodule update --init --recursive
 
 
 pip3 install .
+pip3 install --upgrade 'protobuf>=6.31.1'
+apt update && apt install -y graphviz
 
 
-cd ${ASTRA_SIM}
-pip3 install --upgrade 'protobuf==6.31.1'
+cd ${ASTRA_SIM}/..
+git clone https://github.com/astra-sim/symbolic_tensor_graph
+cd symbolic_tensor_graph/
+
+python3 -m venv symb_env
+source symb_env/bin/activate
+pip install numpy sympy python-graphviz protobuf pandas 
+
 ./build/astra_analytical/build.sh
